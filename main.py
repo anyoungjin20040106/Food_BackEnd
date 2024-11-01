aea03b79552d2f9cd2323f7231abb683fa97f9da
안영진 <anyoungjin0106@kbu.ac.kr>
2024-11-01 18:18:00 +0900

    update code

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ db = create_engine("sqlite:///food.db")  # 전역 연결로 설정
 # FastAPI 앱 초기화
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return "테스트"
 
 @app.post("/GetLocal")
-def get_local(lat: str = Form(...), lon: str = Form(...), kind: str = Form("")):
+def get_local(lat: str = Form(...), lon: str = Form(...), category: str = Form(...)):
     # 위도와 경도를 float으로 변환
     lat = float(lat)
     lon = float(lon)
@@ -27,16 +24,23 @@ def get_local(lat: str = Form(...), lon: str = Form(...), kind: str = Form("")):
     print(region)
     
     # 기본 쿼리와 파라미터 설정
-    query = "SELECT 시설명 FROM food WHERE 지역명=:local"
+    query = "SELECT name FROM food WHERE local=:local"
     params = {'local': region}
     
     # `kind`가 주어지면 카테고리 필터 추가
-    if kind:
-        query += " AND 카테고리=:kind"
-        params['kind'] = kind
+    if category!="상관없음":
+        query += " AND category=:category"
+        params['category'] = category
     
     # SQL 쿼리 실행
     df = pd.read_sql(text(query), con=db, params=params)
     
     # 결과를 리스트로 반환
-    return {'foods':list(df['시설명'])}
+    return {'datas':list(df['name'])}
+
+@app.get("/GetCategory")
+def root():
+    query = "SELECT category FROM food"
+    df = pd.read_sql(text(query), con=db)
+    print(list(df['category'].unique()))
+    return {'datas':["상관없음"]+list(df['category'].unique())}
